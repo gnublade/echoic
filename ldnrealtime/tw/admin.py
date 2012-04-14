eec07784c877b155dbcd0db67142d661f6bc3778
Andy Kilner <andy@andykilner.co.uk>
2012-04-14 18:59:56 +0100

    Recording admin list display.

--- ldnrealtime/tw/admin.py
+++ ldnrealtime/tw/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Recording
 
-admin.site.register(Recording)
+class RecordingAdmin(admin.ModelAdmin):
+
+    list_display = ('recorded_at', 'recorded_by', 'url', 'duration')
+
+admin.site.register(Recording, RecordingAdmin)
