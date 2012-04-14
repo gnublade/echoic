f4339c618afbed9071e4c981ecf5c59b56734d53
Andy Kilner <andy@andykilner.co.uk>
2012-04-14 18:59:36 +0100

    Call, hangup.

--- ldnrealtime/views.py
+++ ldnrealtime/views.py
@@ -1,4 +1,11 @@
+from django.conf import settings
 from django.shortcuts import render
 
+from twilio.util import TwilioCapability
+
 def home(request):
-    return render(request, 'index.html')
+    capability = TwilioCapability(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+    capability.allow_client_outgoing(settings.TWILIO_APP_ID)
+    return render(request, 'index.html', {
+        'token': capability.generate()
+    })
