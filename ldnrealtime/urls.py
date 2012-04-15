8af06c6bbef866f34968d79665e565d6862040c9
Andy Kilner <andy@andykilner.co.uk>
2012-04-15 01:32:36 +0100

    Add simple pusher presence.

--- ldnrealtime/urls.py
+++ ldnrealtime/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'ldnrealtime.views.home', name='home'),
+urlpatterns = patterns('ldnrealtime.views',
+    url(r'^$', 'home', name='home'),
+    url(r'^pusher/auth', 'pusher_auth', name='pusher_auth'),
     url(r'^_tw/', include('ldnrealtime.tw.urls', namespace='tw')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
--- ldnrealtime/views.py
+++ ldnrealtime/views.py
@@ -1,6 +1,13 @@
+import time
+import json
+                                                                                 
 from django.conf import settings
+from django.http import HttpResponse, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.shortcuts import render
 
+from django_pusher.push import pusher
 from twilio.util import TwilioCapability
 
 def home(request):
@@ -9,3 +16,13 @@ def home(request):
     return render(request, 'index.html', {
         'token': capability.generate()
     })
+
+
+@require_POST
+@csrf_exempt
+def pusher_auth(request):
+    channel = request.POST["channel_name"]
+    socket_id = request.POST["socket_id"]
+                                                                                 
+    r = pusher._real_getitem(channel).authenticate(socket_id, {'user_id': socket_id})
+    return HttpResponse(json.dumps(r), mimetype="application/json")
